b31e3981c7bf32b009d5d35aa7ed3c83e911a38c
mdcreator <63508140+mdcreator@users.noreply.github.com>
2022-06-26 23:20:37 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Container from "./components/Container";
+import Container from './components/Container';
+import React from 'react';
+import SearchForm from './components/SearchForm';
 
 export default function App() {
   return (
     <div>
-      <Container/>
+      <Container>
+        <SearchForm />
+      </Container>
     </div>
   );
 }
-
